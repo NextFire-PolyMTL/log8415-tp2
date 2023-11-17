7f62e92ce3cfee046f3ce803a663e0a968da0434
aurpur <122250820+aurelikama@users.noreply.github.com>
2023-11-17 15:50:34 -0500

    add type

--- tests/__main__.py
+++ tests/__main__.py
@@ -7,5 +7,5 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     logging.basicConfig(level="INFO")
-    with Pool(5) as p:
+    with Pool(processes=5) as p:
         p.map(scenario.my_test, range(5))
